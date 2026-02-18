0486633aa5f56b4bffb76bc9b5ebfd1bb9da93a6
Ali-Al-Noufal <alialnoufal3@gmail.com>
2026-02-18 11:53:56 -0500

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,24 @@
+import axios from "axios"
+import { use, useEffect, useState } from "react"
+import MessageForm from "./MessageForm"
+import { useNavigate } from "react-router-dom"
 
 
 const Contact = ({items}) => {
+  const [data,setData]=useState({})
+  const navigate=useNavigate()
+useEffect(()=>{
+if(data.name){
+    axios.post("https://ui-ux-portfolio-alpha.vercel.app/api/api/guest/1/messages",data,{
+        headers:{
+        "Accept":"application/json"
+      }
+}).then(res=>{console.log(res),alert(res.data.message),navigate(0)})
+.catch(err=>console.log(err))
+}
+  
+},[data])
+
   return (
     <div className="p-[5%] h-200 md:h-screen">
 <div className="w-full flex flex-col justify-between items-start pb-10">
@@ -23,16 +41,7 @@ const Contact = ({items}) => {
   )
 })}
 </div>
-<form className="w-full md:w-[65%] mt-5 md:mt-0 h-80 md:h-full flex flex-col justify-between relative">
-<div className="w-full h-14 flex justify-between">
-<input type="text" placeholder="Full Name" className="border w-[48%] h-full border-[#BEC0BF] px-3 text-[#92929D] text-[16px] font-normal rounded-lg " />
-<input type="text" placeholder="Your email" className="border w-[48%] h-full border-[#BEC0BF] px-3 text-[#92929D] text-[16px] font-normal rounded-lg " />
-</div>
-<input type="number" placeholder="Phone number" className="border w-full h-14 border-[#BEC0BF] px-3 text-[#92929D] text-[16px] font-normal rounded-lg "/>
-<textarea placeholder="Message" className="border w-full h-[50%] border-[#BEC0BF] py-3 px-7 text-[#92929D] text-[16px] font-normal rounded-lg " ></textarea>
-<input type="submit" value="Send Message" className="hover:bg-[#1D2130] hover:border-[#FFFFFF] dark:hover:border-[#FFFFFF] hover:text-[#FFFFFF] h-14 bg-[#0C96E2] border border-[#0C96E2] rounded-lg text-[#FFFFFF] font-medium text-[14px] absolute bottom-[-25%] w-50 text-center" />
-</form>
-
+<MessageForm changeData={setData} />
 </div>
     </div>
   )
